c77f9d83a2f145db97dcd601df5adf7cd0f8f50e
Jason Breedlove <j@jasonbreedlove.com>
2023-12-19 17:53:16 -0800

    create display/message, displayHit, and displayMiss functions

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const view = {
+  displayMessage: function (msg) {
+    const messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
